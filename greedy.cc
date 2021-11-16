16b058167f240f19beb6b5beb52f5c342a44a1ea
= <eva.m.szlifierz@est.fib.upc.edu>
2021-11-16 19:08:29 +0100

    comit

--- greedy.cc
+++ greedy.cc
@@ -3,29 +3,30 @@
 using namespace std;
 
 
-typedef vector < vector<int> > Graf;
+typedef vector<vector<int> > Graf;
 
 // 	Pre : Cada vertex ha de tenir un identificador corresponent a un numero de 
 // 	0 a NumVert-1, i hi ha NumVert vertexs al graf.
 //	Post: G conté la llista de vertexs que son adjacents a cada vertex, de 0 a NumVert.
-void llegirGraf(Graf& G, NumVert) {
+//	G estara representat amb una llista d'adjacencies.
+void llegirGraf(Graf& G, int NumVert) {
 	for(int i = 0; i < NumVert; ++i) { // Els vertexs van numerats del 0 a NumVert - 1
 		int U; 
 		cin >> U;
 		while(U != -1) { // Mentres siguin diferents seguim amb la mateixa llista
-			G[i].pushback(U);
+			G[i].push_back(U);
 			cin >> U;
 		}
 	}
 }
 
 void OrdenarVertexsGrauAscendent() {
-	
+
 }
 
 
-main() {
-	int NumVert, NumArestes;
+int main() {
+	int NumVert;
 	cin >> NumVert;
 	Graf G(NumVert);
 	llegirGraf(G, NumVert);
